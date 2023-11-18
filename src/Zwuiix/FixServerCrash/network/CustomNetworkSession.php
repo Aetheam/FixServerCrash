430f11cd00e51ecfce93341f193cc1aad350b355
Zwuiix-cmd <enzo53cochin@gmail.com>
2023-11-18 01:03:01 +0000

    Update CustomNetworkSession.php

--- src/Zwuiix/FixServerCrash/network/CustomNetworkSession.php
+++ src/Zwuiix/FixServerCrash/network/CustomNetworkSession.php
@@ -41,14 +41,47 @@ class CustomNetworkSession extends NetworkSession
             parent::createPlayer();
             return;
         }
-
-        $playerCreated = $this->onPlayerCreated(...);
+        
         $callable = function() : void{
             ReflectionUtils::getProperty(NetworkSession::class, $this, "logger")->error("Failed to create player");
             $this->disconnectWithError(KnownTranslationFactory::pocketmine_disconnect_error_internal());
         };
         ReflectionUtils::getProperty(NetworkSession::class, $this, "server")->createPlayer($this, ReflectionUtils::getProperty(NetworkSession::class, $this, "info"), ReflectionUtils::getProperty(NetworkSession::class, $this, "authenticated"), ReflectionUtils::getProperty(NetworkSession::class, $this, "cachedOfflinePlayerData"))->onCompletion(
-            $playerCreated,
+            function(Player $player) : void{
+                if(!$this->isConnected()){
+                    //the remote player might have disconnected before spawn terrain generation was finished
+                    return;
+                }
+                ReflectionUtils::setProperty(NetworkSession::class, $this, "player", $player);
+                if(!CustomServer::addOnlinePlayer($player)){
+                    return;
+                }
+
+                ReflectionUtils::setProperty(NetworkSession::class, $this, "invManager", new InventoryManager($player, $this));
+
+                $effectManager = ReflectionUtils::getProperty(NetworkSession::class, $this, "player")->getEffects();
+                $effectManager->getEffectAddHooks()->add($effectAddHook = function(EffectInstance $effect, bool $replacesOldEffect) : void{
+                    ReflectionUtils::getProperty(NetworkSession::class, $this, "entityEventBroadcaster")->onEntityEffectAdded([$this], ReflectionUtils::getProperty(NetworkSession::class, $this, "player"), $effect, $replacesOldEffect);
+                });
+                $effectManager->getEffectRemoveHooks()->add($effectRemoveHook = function(EffectInstance $effect): void{
+                    ReflectionUtils::getProperty(NetworkSession::class, $this, "entityEventBroadcaster")->onEntityEffectRemoved([$this], ReflectionUtils::getProperty(NetworkSession::class, $this, "player"), $effect);
+                });
+                ReflectionUtils::getProperty(NetworkSession::class, $this, "disposeHooks")->add(static function() use ($effectManager, $effectAddHook, $effectRemoveHook) : void{
+                    $effectManager->getEffectAddHooks()->remove($effectAddHook);
+                    $effectManager->getEffectRemoveHooks()->remove($effectRemoveHook);
+                });
+
+                $permissionHooks = ReflectionUtils::getProperty(NetworkSession::class, $this, "player")->getPermissionRecalculationCallbacks();
+                $permissionHooks->add($permHook = function() : void{
+                    ReflectionUtils::getProperty(NetworkSession::class, $this, "logger")->debug("Syncing available commands and abilities/permissions due to permission recalculation");
+                    $this->syncAbilities(ReflectionUtils::getProperty(NetworkSession::class, $this, "player"));
+                    $this->syncAvailableCommands();
+                });
+                ReflectionUtils::getProperty(NetworkSession::class, $this, "disposeHooks")->add(static function() use ($permissionHooks, $permHook) : void{
+                    $permissionHooks->remove($permHook);
+                });
+                ReflectionUtils::invoke(NetworkSession::class, $this, "beginSpawnSequence");
+            },
             $callable
         );
     }
